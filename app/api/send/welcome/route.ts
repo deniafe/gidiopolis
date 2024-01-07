3ad5d265c9f8fba26cb45e695f1c53ea8c5e437b
deniafe~ <detoyin@yahoo.com>
2024-01-07 12:16:27 +0100

    Fix some errors 5

--- app/api/send/welcome/route.ts
+++ app/api/send/welcome/route.ts
@@ -2,7 +2,7 @@ import { WelcomeEmailTemplate } from '../../../../components/emails/WelcomeEmail
 import { Resend } from 'resend'
 import * as React from 'react'
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request: Request) {
   const req = await request.json();
