45ae3f719763d5c59a40f22d326992a770c1706b
deniafe~ <detoyin@yahoo.com>
2024-01-30 14:27:03 +0100

    Fixed errors in email

---
+++ app/api/send/welcome/route.ts
@@ -0,0 +1,55 @@
+// import { WelcomeEmailTemplate } from '../../../../components/emails/WelcomeEmail'
+// import { Resend } from 'resend'
+// import * as React from 'react'
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function POST(request: Request) {
+//   const req = await request.json();
+ 
+//   const { name, email } = req; 
+
+//   if (!email || !name) {
+//     return Response.json({ message: 'Missing fields' }, { status: 400 });
+//   }
+ 
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Debby - Gidiopolis <debby@gidiopolis.com>',
+//       to: [email],
+//       subject: 'Welcome To Gidiopolis',
+//       react: WelcomeEmailTemplate({ name, href: 'https://gidiopolis.com/' }) as React.ReactElement,
+//     });
+
+//     return Response.json(data);
+
+//   } catch (error) {
+//     console.log('There was an error sending the email', error)
+//     return Response.json({ error });
+//   }
+// }
+
+
+import { WelcomeEmailTemplate } from '../../../../components/emails/WelcomeEmail'
+import { Resend } from 'resend'
+import * as React from 'react'
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+
+  try {
+    const data = await resend.emails.send({
+      from: 'Debby - Gidiopolis <debby@gidiopolis.com>',
+      to: ['deniafe@gmail.com'],
+      subject: 'Welcome To Gidiopolis',
+      react: WelcomeEmailTemplate({ name: 'Toyin', href: 'https://gidiopolis.com/' }) as React.ReactElement,
+    });
+
+    return Response.json(data);
+
+  } catch (error) {
+    console.log('There was an error sending the email', error)
+    return Response.json({ error });
+  }
+}
